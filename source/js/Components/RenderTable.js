099d7be5e0cbd82d6ed136c72a63e89cd97e3fbe
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-11-30 17:18:44 +0100

    Responsive table, bold links

--- source/js/Components/RenderTable.js
+++ source/js/Components/RenderTable.js
@@ -13,8 +13,8 @@ class RenderTable extends React.Component {
                 <table className="table table-striped" items={paginatedItems}>
                     <tbody>
                     <tr>
-                        <th>{ModularityAgreementsArchiveObject.translation.supplier}</th>
-                        <th>{ModularityAgreementsArchiveObject.translation.category}</th>
+                        <th className="hidden-xs">{ModularityAgreementsArchiveObject.translation.supplier}</th>
+                        <th className="hidden-xs">{ModularityAgreementsArchiveObject.translation.category}</th>
                         <th className="number hidden-xs">{ModularityAgreementsArchiveObject.translation.number}</th>
                         <th className="valid-from hidden-xs">{ModularityAgreementsArchiveObject.translation.fromdate}</th>
                         <th className="valid-to hidden-xs">{ModularityAgreementsArchiveObject.translation.todate}</th>
@@ -23,19 +23,17 @@ class RenderTable extends React.Component {
                     {paginatedItems.map((item, i) => (
                         (item.Supplier.Name) ?
                             <tr key={'row' + item.Id}>
-                                <td key={'supplierName_' + item.Id}><a
+                                <td key={'supplierName_' + item.Id}><span className="hidden-md">{ModularityAgreementsArchiveObject.translation.supplier}: &nbsp;</span> <a
                                     data-id={item.Id}
                                     onClick={ (e) =>
                                         this.props.single(e, item.Id)
                                     }
-                                    href="#">{item.Supplier.Name}</a></td>
-                                <td key={'category_' + item.Id}>{item.Category}</td>
-                                <td className="hidden-xs" key={'number_' + item.Id}>{item.Number}</td>
-                                <td className="hidden-xs"
-                                    key={'validFrom_' + item.Id}>{item.ValidFrom.replace('T00:00:00', '')}</td>
-                                <td className="hidden-xs"
-                                    key={'validTo_' + item.Id}>{item.ValidTo.replace('T00:00:00', '')}</td>
-                                <td className="hidden-xs" key={'buyerName_' + item.Id}>{item.Buyer.Name}</td>
+                                    href="#"><b>{item.Supplier.Name}</b></a></td>
+                                <td key={'category_' + item.Id}><span className="hidden-md">{ModularityAgreementsArchiveObject.translation.category}: &nbsp;</span>{item.Category}</td>
+                                <td key={'number_' + item.Id}><span className="hidden-md">{ModularityAgreementsArchiveObject.translation.number}: &nbsp;</span>{item.Number}</td>
+                                <td key={'validFrom_' + item.Id}><span className="hidden-md">{ModularityAgreementsArchiveObject.translation.fromdate}: &nbsp;</span>{item.ValidFrom.replace('T00:00:00', '')}</td>
+                                <td key={'validTo_' + item.Id}><span className="hidden-md">{ModularityAgreementsArchiveObject.translation.todate}: &nbsp;</span>{item.ValidTo.replace('T00:00:00', '')}</td>
+                                <td key={'buyerName_' + item.Id}><span className="hidden-md">{ModularityAgreementsArchiveObject.translation.contractsManager}: &nbsp;</span>{item.Buyer.Name}</td>
                             </tr>
                             : ''
                     ))}
