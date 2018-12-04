51cc4a03996438ac1d8f4afebb69f8a92da5cbe0
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-12-04 15:45:50 +0100

    scss, back compability

--- source/js/Components/Search.js
+++ source/js/Components/Search.js
@@ -39,7 +39,7 @@ class Search extends React.Component {
                 </div>
                 {(this.props.search) ?
                     <div className="searchResult">
-                        {ModularityAgreementsArchiveObject.translation.yoursearch}&nbsp;<b>{this.props.searchInput}</b> {ModularityAgreementsArchiveObject.translation.gave}
+                        {ModularityAgreementsArchiveObject.translation.yoursearch}&nbsp;<b>{this.props.searchInput}</b> {ModularityAgreementsArchiveObject.translation.gave}&nbsp;
                         <b>{this.props.totalItems}</b>&nbsp;{ModularityAgreementsArchiveObject.translation.hits}&nbsp;<b>{this.props.current}</b>&nbsp;
                         {ModularityAgreementsArchiveObject.translation.of}&nbsp;<b>{this.props.totalPages}</b></div>
                     : ''}
