436a3787cd6618627597158c32ce16b14181e14c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2018-12-06 16:20:55 +0100

    Convert description to sanitized html

--- source/php/Api.php
+++ source/php/Api.php
@@ -103,7 +103,37 @@ class Api
 
                 //Clean text content
                 if (isset($item->Description)) {
+
+                    //Remove br's
                     $item->Description = preg_replace('/<br\s?\/?>/ius', "\n", str_replace("\n", "", str_replace("\r", "", htmlspecialchars_decode($item->Description))));
+
+                    //DEcode html entities
+
+
+                    if ($data = explode("\n", $item->Description)) {
+
+                        if (is_array($data) && !empty($data)) {
+
+                            //Remove empty explodes
+                            $data = array_filter($data);
+
+                            //Detect uppercase, handle them as titles
+                            foreach ($data as &$element) {
+                                if ($element == mb_strtoupper($element)) {
+                                    $element = '<h2>' . ucfirst(mb_strtolower($element)) . '</h2>';
+                                }
+                            }
+
+                            //Smack it togheter again
+                            $item->Description = implode("\n\n", $data);
+                        }
+                    }
+
+                    //The content filter
+                    $item->Description = apply_filters('the_content', $item->Description);
+
+                    //Decode html
+                    $item->Description = html_entity_decode($item->Description);
                 }
 
                 //Remove url
